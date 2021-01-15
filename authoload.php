114eb18258932ace0cc41af8bf97830470fabef3
webazex <40128990+webazex@users.noreply.github.com>
2021-01-15 22:47:44 +0200

    add-require files classes and write class App
    Начата работа по созданию методов для класса App

--- authoload.php
+++ authoload.php
@@ -1,8 +1,34 @@
 <?php
-spl_autoload_register(function ($class_name) {
-    $plugin_dir =  plugin_dir_path(__FILE__);
-    $str = $plugin_dir.$class_name . '.php';
-    $str = str_replace("\\", "/", $str);
-//    echo $str;
-    require_once ($str);
-});
+//spl_autoload_register(function ($class_name) {
+//    $plugin_dir =  plugin_dir_path(__FILE__);
+//    $str = $plugin_dir.$class_name . '.php';
+//    $str = str_replace("\\", "/", $str);
+////    echo $str;
+//    require_once ($str);
+//});
+
+function getPatch($type, $class)
+{
+    $patches = array(
+        'controllers' => 'Core/Controllers/',
+        'models' => 'Core/Models/',
+        'views' => 'Core/Views/'
+    );
+    if ($type === "v"):
+        $req = plugin_dir_path(__FILE__) . $patches['views'] . $class . '.php';
+        $str = str_replace("\\", "/", $req);
+        return $str;
+    elseif ($type === "c"):
+        $req = plugin_dir_path(__FILE__) . $patches['controllers'] . $class . '.php';
+        $str = str_replace("\\", "/", $req);
+        return $str;
+    elseif ($type === "m"):
+        $req = plugin_dir_path(__FILE__) . $patches['models'] . $class . '.php';
+        $str = str_replace("\\", "/", $req);
+        return $str;
+    else:
+        exit("Путь не найден");
+    endif;
+}
+$str = getPatch('c', 'App');
+require_once $str;
