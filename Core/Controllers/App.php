5dfb2357014dccfbf578fb628bb1a2912f83029c
webazex <40128990+webazex@users.noreply.github.com>
2021-01-15 22:52:01 +0200

    edit App
    add new methods

--- Core/Controllers/App.php
+++ Core/Controllers/App.php
@@ -6,7 +6,16 @@ namespace Core\Controllers;
 
 class App
 {
+    function __construct()
+    {
+        add_action('admin_menu',  array (&$this, 'dashboard') );
+    }
+
     static function start(){
-        echo "start";
+        add_option('r_user_discount', 10);
+        add_option('exit_discount', 3);
+    }
+    static function dashboard(){
+        add_options_page('page_title', 'menu_title', 8, 'file', 'func');
     }
 }
