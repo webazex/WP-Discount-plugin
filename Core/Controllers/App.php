1ca0ad0fc3bc64707912999f0043100ec22e7134
webazex <40128990+webazex@users.noreply.github.com>
2021-01-18 02:22:12 +0200

    add new app

--- Core/Controllers/App.php
+++ Core/Controllers/App.php
@@ -3,19 +3,56 @@
 
 namespace Core\Controllers;
 
+use Core\Models\DB;
+use WC_Product_Simple;
 
 class App
 {
-    function __construct()
+    static function start()
     {
-        add_action('admin_menu',  array (&$this, 'dashboard') );
-    }
 
-    static function start(){
+        add_option('r_user_discount_def', 10);
+        add_option('exit_discount_def', 3);
+
+        self::sql_install();
         add_option('r_user_discount', 10);
         add_option('exit_discount', 3);
     }
-    static function dashboard(){
+
+    function sql_install()
+    {
+        $db = new DB();
+        $db->create_plugin_table();
+        $db->set_date(30, 10);
+    }
+
+    static function dashboard()
+    {
         add_options_page('page_title', 'menu_title', 8, 'file', 'func');
     }
+
+    function getPercent($quit = false)
+    {
+        $db = new DB();
+        if (is_user_logged_in()):
+            $percent = $db->getValue('1');
+        elseif ($quit == true):
+            $percent = $db->getValue('2');
+        else:
+            $percent = "not percent";
+        endif;
+        return $percent;
+    }
+
+    function getDiscount($discount)
+    {
+        if ($discount == "not percent"):
+            return "Скидка отсутствует";
+        else:
+//            $product_id = get_the_ID();
+//            $product = wc_get_product( 10 );
+//            echo $product->get_sale_price();
+        endif;
+    }
+
 }
